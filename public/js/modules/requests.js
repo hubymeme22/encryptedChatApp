16837e4216902dc1f9e514509532f58ff2980826
HueHueberry <huby.new1112@gmail.com>
2022-12-31 19:59:08 +0800

    Fixed POST request error

--- public/js/modules/requests.js
+++ public/js/modules/requests.js
@@ -5,32 +5,32 @@ const rejectCallbackFormat = (error) => {};
 
 // make a normal post request to the server
 function postRequest(route='/', data={}, accepted_callback=acceptCallbackFormat, reject_callback=rejectCallbackFormat) {
-    const request = fetch(url + route, {
+    fetch(url + route, {
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data)
-    });
+        body: JSON.stringify(data)})
 
-    request.then((response) => { return response.json() });
-    request.then(accepted_callback);
-    request.catch(reject_callback);
+    .then(response => { return response.json() } )
+    .then(accepted_callback)
+    .catch(reject_callback);
 }
 
 // make a post request to the server with modified header "token"
 // this one is used for authorization
 function postTokenRequest(route='/', token='', data={}, accepted_callback=acceptCallbackFormat, reject_callback=rejectCallbackFormat) {
-    const request = fetch(url + route, {
+    fetch(url + route, {
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'token': token
         },
-        body: JSON.stringify(data)
-    });
+        body: JSON.stringify(data)})
 
-    request.then((response) => { return response.json() });
-    request.then(accepted_callback);
-    request.catch(reject_callback);
+    .then(response => { return response.json() } )
+    .then(accepted_callback)
+    .catch(reject_callback);
 }
 
 export {
