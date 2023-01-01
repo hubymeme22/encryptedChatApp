9cbfae0c73eb4181d0a4df27bc01a74aa55fbf8d
HueHueberry <huby.new1112@gmail.com>
2023-01-01 15:48:58 +0800

    Added validation on middleware

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,2 +1,27 @@
 // this module contains middlewares that will be used for
 // different type of validation.
+
+// validates the parameters for registering account
+function validateRegister(req, res, next) {
+    const creds = req.body;
+    if (creds.username == null ||
+        creds.password == null ||
+        creds.username == '' ||
+        creds.password == '') {
+
+        // send a malformed response
+        res.status(400);
+        res.json({
+            created: false,
+            existing: false,
+            data: null,
+            error: null,
+        });
+    } else {
+        next();
+    }
+}
+
+module.exports = {
+    validateRegister
+};
